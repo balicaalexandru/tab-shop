1a9dee435b8f4d61b5be4d4d696b1ee2f4dad39b
Balica Alexandru <alexbalica2@gmail.com>
2023-11-08 15:13:41 +0200

    Working car parts shop

---
+++ src/products.js
@@ -0,0 +1,58 @@
+import product1 from "./assets/engine.png";
+import product2 from "./assets/gearbox.png";
+import product3 from "./assets/turbo.jpg";
+import product4 from "./assets/exhaust.png";
+import product5 from "./assets/suspension.png";
+import product6 from "./assets/brakes.png";
+import product7 from "./assets/intake.png";
+import product8 from "./assets/tires.png";
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "MB 6.3 Engine",
+    price: 10999,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Gearbox",
+    price: 4999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Turbine",
+    price: 2999,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Exhaust",
+    price: 1599,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Suspension",
+    price: 1399,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Brakes",
+    price: 1099,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Air Filter",
+    price: 399,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Set of tires",
+    price: 999,
+    productImage: product8,
+  },
+];
